235dfb1deb0cb58b044fc1f388650fe99c0002b2
bredickerson35@gmail.com <bredickerson35@gmail.com>
2023-10-25 16:05:43 -0400

    more javascript stuff

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,32 @@
 "use strict"
 
-let additionForm = document.getElementById("additionForm")
-additionForm.onsubmit = calculateSum
+window.onload = function (event) {
+    let additionForm = document.getElementById("additionForm")
+    additionForm.onsubmit = calculateSum
 
-function calculateSum (event) {
+    let addressForm = document.getElementById("addressForm")
+    addressForm.onsubmit = addressResult
+    
+    let ghostBoy = document.getElementById("👻")
+    ghostBoy.onmouseover = flipGhost
+    
+
+}
+
+document.onkeydown = function (event) {
+    console.log(event)
+
+}
+
+function flipGhost (event) {
+    event.target.classList.toggle("flipped")
+    console.log("ghost should be flipped.")
+}
+
+function calculateSum(event) {
     event.preventDefault()
+
+    console.dir(event.target)
     let x = event.target.elements.leftOperand.valueAsNumber
     let y = event.target.elements.rightOperand.valueAsNumber
     console.log(x)
@@ -12,23 +34,21 @@ function calculateSum (event) {
     let sum = x + y
 
     let resultDiv = document.getElementById("additionResult")
-     resultDiv.innerHTML = `<h4>${x} + ${y} = <span class= "display-result-red"> ${sum}</span></h4>`
+    resultDiv.innerHTML = `<h4>${x} + ${y} = <span class= "display-result-red"> ${sum}</span></h4>`
 }
 
-let addressForm = document.getElementById("addressForm")
-addressForm.onsubmit = addressResult
-
-function addressResult (event){
+function addressResult(event) {
     event.preventDefault()
-    let city = event.target.elements.leftOperand.value
-    let state = event.target.elements.middleOperand.value
-    let country = event.target.elements.rightOperand.value
+
+    console.dir(event.target)
+    let city = event.target.elements.city.value
+    let state = event.target.elements.state.value
+    let country = event.target.elements.country.value
     console.log(city)
     console.log(state)
     console.log(country)
-    let submit = (city , state  , country)
+    let submit = (city, state, country)
 
-    let addressResult = document.getElementById(addressResult)
-    addressResult.innerHTML = `<h4>{city}, {state}, {country}</span></h3>`
-
+    let addressResult = document.getElementById("addressResult")
+    addressResult.innerHTML = `<h4>Address:<span class="display-result-red"> ${city} , ${state}, ${country}</span></h4>`
 }
